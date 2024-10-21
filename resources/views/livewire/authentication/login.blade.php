f5ca48acf16db447ddcb04ab8b638a6668307c6c
printf_cherif <printf0cherif@gmail.com>
2024-10-21 22:36:09 +0000

    Mise a jour du responsive design

--- resources/views/livewire/authentication/login.blade.php
+++ resources/views/livewire/authentication/login.blade.php
@@ -1,4 +1,5 @@
-<section class="flex flex-col items-center justify-center px-6 pb-8 mx-auto md:h-screen lg:py-0">
+<section
+    class="flex flex-col h-[650px] align-items-center items-center justify-center px-6 pb-8 mx-auto md:h-screen lg:py-0">
     <div
         class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
         <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
