20b61295afc7f35b33b9d3b98fa9707f7ef80f3b
printf_cherif <printf0cherif@gmail.com>
2024-10-22 23:29:27 +0000

    Creation de la side bar

--- resources/views/livewire/chat/conversation.blade.php
+++ resources/views/livewire/chat/conversation.blade.php
@@ -1,3 +1,8 @@
-<div class="bg-red-500 h-[35em] rounded-lg px-10 py-4 basis-1/4">
+<div class="bg-red-500 h-[35em] rounded-lg px-10 py-4 basis-1/4 flex flex-col">
     <h1 class="text-xl font-bold text-center">Conversations</h1>
+    <div class="flex flex-row">
+        <button>+</button>
+        <button>-</button>
+        <button>=</button>
+    </div>
 </div>
